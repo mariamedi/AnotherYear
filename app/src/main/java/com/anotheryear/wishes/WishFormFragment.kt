7ac77568dfb9888c1785e7c171cad16693333aa6
AlejandraGarza42 <agarza@wpi.edu>
2020-10-11 15:19:27 -0400

    Moved to fragment

--- app/src/main/java/com/anotheryear/wishes/WishFormFragment.kt
+++ app/src/main/java/com/anotheryear/wishes/WishFormFragment.kt
@@ -39,37 +39,130 @@ class WishFormFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_wish_form, container, false)
 
-//        // Initialization of UI Elements
-//        birthPersonName = view.findViewById(R.id.PW_their_name) as EditText
-//        relationshipBar = view.findViewById(R.id.PW_simpleSeekBar) as SeekBar
-//        yourName = view.findViewById(R.id.PW_your_name) as EditText
-//        noSignature = view.findViewById(R.id.PW_no_signature) as CheckBox
-//        generateWishButton = view.findViewById(R.id.PW_generate_wish_button) as Button
-//
-//        //listener for SeekBar
-//        relationshipBar.setOnClickListener { view: View ->
-//            wishViewModel.relationship = relationshipBar.progress
-//        }
-//
-//        //listener for GenerateWish Button
-//        generateWishButton.setOnClickListener{view: View->
-//            //check that all field have been filled out
-//            if(wishViewModel.readyForWish()){
-//                //generate wish activity
-//                Toast.makeText(
-//                    activity,
-//                    "Time to generate wish!",
-//                    Toast.LENGTH_SHORT).show()
-//            } else {
-//                //tell user to fill out all fields
-//                Toast.makeText(
-//                    activity,
-//                    R.string.PW_error,
-//                    Toast.LENGTH_SHORT).show()
-//            }
-//
-//        }
+        // Initialization of UI Elements
+        birthPersonName = view.findViewById(R.id.PW_their_name) as EditText
+        relationshipBar = view.findViewById(R.id.PW_simpleSeekBar) as SeekBar
+        yourName = view.findViewById(R.id.PW_your_name) as EditText
+        noSignature = view.findViewById(R.id.PW_no_signature) as CheckBox
+        generateWishButton = view.findViewById(R.id.PW_generate_wish_button) as Button
+
+        //listener for SeekBar
+        relationshipBar.setOnClickListener { view: View ->
+            wishViewModel.relationship = relationshipBar.progress
+        }
+
+        //listener for GenerateWish Button
+        generateWishButton.setOnClickListener{view: View->
+            //check that all field have been filled out
+            if(wishViewModel.readyForWish()){
+                //generate wish activity
+                Toast.makeText(
+                    activity,
+                    "Time to generate wish!",
+                    Toast.LENGTH_SHORT).show()
+            } else {
+                //tell user to fill out all fields
+                Toast.makeText(
+                    activity,
+                    R.string.PW_error,
+                    Toast.LENGTH_SHORT).show()
+            }
+
+        }
+
+        // listener for birthday person name
+        birthPersonName.addTextChangedListener(object : TextWatcher {
+
+            override fun afterTextChanged(s: Editable) {}
+
+            override fun beforeTextChanged(s: CharSequence, start: Int,
+                                           count: Int, after: Int) {
+            }
+            override fun onTextChanged(s: CharSequence, start: Int,
+                                       before: Int, count: Int) {
+                wishViewModel.theirName = s.toString()
+            }
+        })
+
+        //listener for birthday person name
+        yourName.addTextChangedListener(object : TextWatcher {
+
+            override fun afterTextChanged(s: Editable) {}
+
+            override fun beforeTextChanged(s: CharSequence, start: Int,
+                                           count: Int, after: Int) {
+            }
+            override fun onTextChanged(s: CharSequence, start: Int,
+                                       before: Int, count: Int) {
+                wishViewModel.yourName = s.toString()
+            }
+        })
+
+        // checking if the user does not want a Signature
+        noSignature.setOnClickListener{view: View->
+            onCheckboxClicked(view)
+        }
+
         return view
     }
 
+    /**
+     * Overriding onStart
+     */
+    override fun onStart() {
+        super.onStart()
+
+        //EditText listeners
+        val nameWatcher = object : TextWatcher {
+
+            override fun beforeTextChanged(
+                sequence: CharSequence?,
+                start: Int,
+                count: Int,
+                after: Int
+            ) {
+                // This space intentionally left blank
+            }
+
+            override fun onTextChanged(
+                sequence: CharSequence?,
+                start: Int,
+                before: Int,
+                count: Int
+            ) {
+                val name = sequence.toString()
+            }
+
+            override fun afterTextChanged(sequence: Editable?) {
+                // This one too
+            }
+        }
+
+        // Listeners
+        birthPersonName.addTextChangedListener(nameWatcher)
+        yourName.addTextChangedListener(nameWatcher)
+
+        // Adding values to ViewModel
+        wishViewModel.theirName = birthPersonName.text.toString()
+        wishViewModel.yourName = yourName.text.toString()
+    }
+
+    /**
+     * Identify if No Signature checkbox was selected
+     */
+    fun onCheckboxClicked(view: View){
+        if(view is CheckBox){
+            val checked: Boolean = view.isChecked
+
+            //disable yourName EditText if checked
+            when(view.id){
+                R.id.PW_no_signature ->{
+                    yourName.isEnabled = !checked
+                    wishViewModel.noSignature = checked
+                }
+            }
+        }
+    }
+
 }
+
--- app/src/main/java/com/anotheryear/wishes/WishesActivity.kt
+++ app/src/main/java/com/anotheryear/wishes/WishesActivity.kt
@@ -19,15 +19,15 @@ import android.widget.SeekBar
  * Activity for all Birthday Wishes functionality
  */
 class WishesActivity : AppCompatActivity() {
-    private lateinit var birthPersonName: EditText
-    private lateinit var relationshipBar: SeekBar
-    private lateinit var yourName: EditText
-    private lateinit var noSignature: CheckBox
-    private lateinit var generateWishButton: Button
-
-    private val wishViewModel: WishesViewModel by lazy {
-        ViewModelProviders.of(this).get(WishesViewModel::class.java)
-    }
+//    private lateinit var birthPersonName: EditText
+//    private lateinit var relationshipBar: SeekBar
+//    private lateinit var yourName: EditText
+//    private lateinit var noSignature: CheckBox
+//    private lateinit var generateWishButton: Button
+//
+//    private val wishViewModel: WishesViewModel by lazy {
+//        ViewModelProviders.of(this).get(WishesViewModel::class.java)
+//    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,80 +43,80 @@ class WishesActivity : AppCompatActivity() {
             bottomNavigationView.selectedItemId = R.id.nav_wish
         }
 
-        // Initialization of UI Elements
-        birthPersonName = findViewById(R.id.PW_their_name) as EditText
-        relationshipBar = findViewById(R.id.PW_simpleSeekBar) as SeekBar
-        yourName = findViewById(R.id.PW_your_name) as EditText
-        noSignature = findViewById(R.id.PW_no_signature) as CheckBox
-        generateWishButton = findViewById(R.id.PW_generate_wish_button) as Button
-
-        //listener for SeekBar
-        relationshipBar?.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
-            override fun onProgressChanged(
-                seek: SeekBar,
-                progress: Int, fromUser: Boolean
-            ) {
-                // write custom code for progress is changed
-            }
-
-            override fun onStartTrackingTouch(seek: SeekBar) {
-                // write custom code for progress is started
-            }
-
-            override fun onStopTrackingTouch(seek: SeekBar) {
-                // write custom code for progress is stopped
-                wishViewModel.relationship = relationshipBar.progress
-            }
-        })
-
-
-
-        //listener for birthday person name
-        birthPersonName.addTextChangedListener(object : TextWatcher {
-
-            override fun afterTextChanged(s: Editable) {}
-
-            override fun beforeTextChanged(s: CharSequence, start: Int,
-                                           count: Int, after: Int) {
-            }
-            override fun onTextChanged(s: CharSequence, start: Int,
-                                       before: Int, count: Int) {
-                wishViewModel.theirName = s.toString()
-            }
-        })
-
-        //listener for birthday person name
-        yourName.addTextChangedListener(object : TextWatcher {
-
-            override fun afterTextChanged(s: Editable) {}
-
-            override fun beforeTextChanged(s: CharSequence, start: Int,
-                                           count: Int, after: Int) {
-            }
-            override fun onTextChanged(s: CharSequence, start: Int,
-                                       before: Int, count: Int) {
-                wishViewModel.yourName = s.toString()
-            }
-        })
-
-        //listener for GenerateWish Button
-        generateWishButton.setOnClickListener{view: View ->
-            //check that all field have been filled out
-            if(wishViewModel.readyForWish()){
-                //generate wish activity
-                Toast.makeText(
-                    this,
-                    "Time to generate wish!",
-                    Toast.LENGTH_SHORT).show()
-            } else {
-                //tell user to fill out all fields
-                Toast.makeText(
-                    this,
-                    R.string.PW_error,
-                    Toast.LENGTH_SHORT).show()
-            }
-
-        }
+//        // Initialization of UI Elements
+//        birthPersonName = findViewById(R.id.PW_their_name) as EditText
+//        relationshipBar = findViewById(R.id.PW_simpleSeekBar) as SeekBar
+//        yourName = findViewById(R.id.PW_your_name) as EditText
+//        noSignature = findViewById(R.id.PW_no_signature) as CheckBox
+//        generateWishButton = findViewById(R.id.PW_generate_wish_button) as Button
+//
+//        //listener for SeekBar
+//        relationshipBar?.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+//            override fun onProgressChanged(
+//                seek: SeekBar,
+//                progress: Int, fromUser: Boolean
+//            ) {
+//                // write custom code for progress is changed
+//            }
+//
+//            override fun onStartTrackingTouch(seek: SeekBar) {
+//                // write custom code for progress is started
+//            }
+//
+//            override fun onStopTrackingTouch(seek: SeekBar) {
+//                // write custom code for progress is stopped
+//                wishViewModel.relationship = relationshipBar.progress
+//            }
+//        })
+//
+//
+//
+//        //listener for birthday person name
+//        birthPersonName.addTextChangedListener(object : TextWatcher {
+//
+//            override fun afterTextChanged(s: Editable) {}
+//
+//            override fun beforeTextChanged(s: CharSequence, start: Int,
+//                                           count: Int, after: Int) {
+//            }
+//            override fun onTextChanged(s: CharSequence, start: Int,
+//                                       before: Int, count: Int) {
+//                wishViewModel.theirName = s.toString()
+//            }
+//        })
+//
+//        //listener for birthday person name
+//        yourName.addTextChangedListener(object : TextWatcher {
+//
+//            override fun afterTextChanged(s: Editable) {}
+//
+//            override fun beforeTextChanged(s: CharSequence, start: Int,
+//                                           count: Int, after: Int) {
+//            }
+//            override fun onTextChanged(s: CharSequence, start: Int,
+//                                       before: Int, count: Int) {
+//                wishViewModel.yourName = s.toString()
+//            }
+//        })
+//
+//        //listener for GenerateWish Button
+//        generateWishButton.setOnClickListener{view: View ->
+//            //check that all field have been filled out
+//            if(wishViewModel.readyForWish()){
+//                //generate wish activity
+//                Toast.makeText(
+//                    this,
+//                    "Time to generate wish!",
+//                    Toast.LENGTH_SHORT).show()
+//            } else {
+//                //tell user to fill out all fields
+//                Toast.makeText(
+//                    this,
+//                    R.string.PW_error,
+//                    Toast.LENGTH_SHORT).show()
+//            }
+//
+//        }
     }
 
     /**
@@ -162,58 +162,58 @@ class WishesActivity : AppCompatActivity() {
     /**
      * Overriding onStart
      */
-    override fun onStart() {
-        super.onStart()
-
-        //EditText listeners
-        val nameWatcher = object : TextWatcher {
-
-            override fun beforeTextChanged(
-                sequence: CharSequence?,
-                start: Int,
-                count: Int,
-                after: Int
-            ) {
-                // This space intentionally left blank
-            }
-
-            override fun onTextChanged(
-                sequence: CharSequence?,
-                start: Int,
-                before: Int,
-                count: Int
-            ) {
-                val name = sequence.toString()
-            }
-
-            override fun afterTextChanged(sequence: Editable?) {
-                // This one too
-            }
-        }
-
-        // Listeners
-        birthPersonName.addTextChangedListener(nameWatcher)
-        yourName.addTextChangedListener(nameWatcher)
-
-        // Adding values to ViewModel
-        wishViewModel.theirName = birthPersonName.text.toString()
-        wishViewModel.yourName = yourName.text.toString()
-    }
-
-    /**
-     * Identify if No Signature checkbox was selected
-     */
-    fun onCheckboxClicked(view: View){
-        if(view is CheckBox){
-            val checked: Boolean = view.isChecked
-
-            //disable yourName EditText if checked
-            when(view.id){
-                R.id.PW_no_signature ->{
-                    yourName.isEnabled = !checked
-                    wishViewModel.noSignature = checked
-                }
-            }
-        }
-    }
+//    override fun onStart() {
+//        super.onStart()
+//
+//        //EditText listeners
+//        val nameWatcher = object : TextWatcher {
+//
+//            override fun beforeTextChanged(
+//                sequence: CharSequence?,
+//                start: Int,
+//                count: Int,
+//                after: Int
+//            ) {
+//                // This space intentionally left blank
+//            }
+//
+//            override fun onTextChanged(
+//                sequence: CharSequence?,
+//                start: Int,
+//                before: Int,
+//                count: Int
+//            ) {
+//                val name = sequence.toString()
+//            }
+//
+//            override fun afterTextChanged(sequence: Editable?) {
+//                // This one too
+//            }
+//        }
+//
+//        // Listeners
+//        birthPersonName.addTextChangedListener(nameWatcher)
+//        yourName.addTextChangedListener(nameWatcher)
+//
+//        // Adding values to ViewModel
+//        wishViewModel.theirName = birthPersonName.text.toString()
+//        wishViewModel.yourName = yourName.text.toString()
+//    }
+//
+//    /**
+//     * Identify if No Signature checkbox was selected
+//     */
+//    fun onCheckboxClicked(view: View){
+//        if(view is CheckBox){
+//            val checked: Boolean = view.isChecked
+//
+//            //disable yourName EditText if checked
+//            when(view.id){
+//                R.id.PW_no_signature ->{
+//                    yourName.isEnabled = !checked
+//                    wishViewModel.noSignature = checked
+//                }
+//            }
+//        }
+//    }
 }
